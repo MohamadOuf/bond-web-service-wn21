5dffdf4748a4458c7920e5b1d5c4623a8290e33e
Yahya Bakhtiar <yahyabakhtiar333@gmail.com>
2021-03-07 20:14:05 -0800

    adds new files

---
+++ data/restaurants-ratings.js
@@ -0,0 +1,91 @@
+{
+	"title": "Restaurants by Rating",
+	"films":[
+			  
+			  
+			  {
+				"Name":"East India Grill",
+				"Phone":"+1 253-529-9292",
+				"address":"34029 Hoyt Rd SW Suite C, Federal Wa31845 Gateway Center Blvd S, Federal Way, WA 98003-5616",
+				"Rating":4.5,
+				"Cuisine":"Indian Cuisine",
+				"Image":"india.jpg"
+			  },
+			  {
+				"Name":"Jimmy Mac's Roadhouse",
+				"Phone":"+1 253-874-6000",
+				"address":"34902 Pacific Hwy S Federal Way Crossings, Federal Way, WA 98003-8380",
+				"Rating":4.0,
+				"Cuisine":"Steakhouss",
+				"Image":"steak.jpg"
+			  },
+
+			  {
+				"Name":"Black Bear Diner",
+				"Phone":"+1 253-945-8332",
+				"address":"32065 Pacific Hwy S, Federal Way, WA 98003-6001",
+				"Rating":4.0,
+				"Cuisine":"Americ",
+				"Image":"black.jpg"
+			  },
+			  {
+				"Name":"Verrazano's Italian Restaurant",
+				"Phone":"+1 253-946-4122",
+				"address":"32065 Pacific Hwy S, Federal Way, WA 98003-6001",
+				"Rating":4.0,
+				"Cuisine":"Italia",
+				"Image":"it.jpg"
+			  },
+			  {
+				"Name":"Thai Bistro",
+				"Phone":"+1 253-874-8800",
+				"address":"334817 Enchanted Pkwy S # 1-102, Federal Way, WA 98003-8376",
+				"Rating":4.0,
+				"Cuisine":"Asia",
+				"Image":"asia.jpg"
+			  },
+			  {
+				"Name":"Pac Island Grill Hawaiian Style BBQ",
+				"Phone":"+1 253-529-8667",
+				"address":"2012 S 320th St Suite H, Federal Way, WA 98003-5454",
+				"Rating":4.5,
+				"Cuisine":"Asia",
+				"Image":"pac.jpg"
+			  },
+			  {
+				"Name":"Tokyo-ya Ramen & Izakaya",
+				"Phone":"+1 206-592-6552",
+				"address":"31507 Pacific Hwy S 31507 Pacific Hwy S, 98003-5405, Federal Way, Washington",
+				"Rating":4.5,
+				"Cuisine":"Japanses",
+				"Image":"ramen.jpg"
+			  },
+			  {
+				"Name":"Los Bigotes de Villa",
+				"Phone":"+1 253-941-2071",
+				"address":"30420 Pacific Hwy S Unit 3, Federal Way, WA 98003-5125",
+				"Rating":4.5,
+				"Cuisine":"Mexican",
+				"Image":"mex.jpg"
+			  },
+			  {
+				"Name":"Ban SomTum Isaan Thai",
+				"Phone":"+1 253-517-3873",
+				"address":"35109 Pacific Hwy S 35109 Pacific Hwy S, 98003-8310, Federal Way, Washington",
+				"Rating":4.5,
+				"Cuisine":"Thai",
+				"Image":"ban.jpg"
+			  },
+			  {
+				"Name":"Mediterranean Gyro Grill",
+				"Phone":"+1 253-874-1144",
+				"address":"34024 Hoyt Rd SW, Federal Way, WA 98023-3224",
+				"Rating":4.5,
+				"Cuisine":"Mediterranean",
+				"Image":"med.jpg"
+			  },
+			 
+
+			
+			]
+}
